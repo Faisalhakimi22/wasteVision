830acf06aa792294183dd40c520b414f822d4c42
Faisal Hakimi <156843443+Faisalhakimi22@users.noreply.github.com>
2024-09-15 19:56:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ page_bg_img = f"""
     left: 50%;
     transform: translateX(-50%);
     font-weight: bold;
-    font-size: 5em; /* Adjust size as needed */
+    font-size: 3em; /* Adjust size as needed */
     color: black; /* Adjust color as needed */
     z-index: 1;
 }}
@@ -81,7 +81,7 @@ page_bg_img = f"""
     }}
     .title {{
         top: 80px; /* Adjust for mobile view */
-        font-size: 3.5em; /* Adjust size for smaller screens */
+        font-size: 2.5em; /* Adjust size for smaller screens */
     }}
 }}
 
