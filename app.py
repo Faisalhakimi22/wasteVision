91e80735c7eaa68251fa22addbc7225903738936
Faisal Hakimi <156843443+Faisalhakimi22@users.noreply.github.com>
2024-09-15 20:00:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ page_bg_img = f"""
 .title {{
     position: absolute;
     top: 100px; /* Adjust position as needed */
-    left: 55%;
+    left: 50%;
     transform: translateX(-50%);
     font-weight: bold;
     font-size: 3em; /* Adjust size as needed */
