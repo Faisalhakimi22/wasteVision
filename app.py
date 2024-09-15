e9b28789d3378720e8792e143015ddaae2f83f85
Faisal Hakimi <156843443+Faisalhakimi22@users.noreply.github.com>
2024-09-15 20:59:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ page_bg_img = f"""
 
 /* Sidebar background for desktop */
 [data-testid="stSidebar"] > div:first-child {{
-    background-image: url("https://plus.unsplash.com/premium_photo-1679728130451-ebba4dc5307d?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-image: url("https://plus.unsplash.com/premium_photo-1664013683829-b9fb1d79e5f3?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-position: center;
     background-repeat: no-repeat;
     background-attachment: fixed;
