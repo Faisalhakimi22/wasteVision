68bace8bd32f9f079be639164bbb3cd6439f1fb9
Faisal Hakimi <156843443+Faisalhakimi22@users.noreply.github.com>
2024-09-15 21:21:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ page_bg_img = f"""
     background-image: url("https://as2.ftcdn.net/v2/jpg/00/67/08/17/1000_F_67081713_yoB2gKhW150YEYMLKxP9VgceF1OGAQLy.jpg");
     background-position: top;
     background-repeat: no-repeat;
+        color: white;
+
 }}
 
 /* Sidebar background for desktop */
